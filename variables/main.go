3bf1ed3ec090f13342118e7e82554095baa3af81
mlobf <marcoslemeborbafilho@gmail.com>
2023-11-16 23:39:39 -0300

    How  to declare a variable - simple sintaxe

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstNumber int
+	firstNumber = 10
+	var secondNumber = 5
+	subtraction := 9
+
+	fmt.Println(firstNumber)
+	fmt.Println(secondNumber)
+	fmt.Println(subtraction)
+}
