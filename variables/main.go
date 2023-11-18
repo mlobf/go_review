14fbff572ae364a584a88c48e51ec5d629024ae2
mlobf <marcoslemeborbafilho@gmail.com>
2023-11-17 22:07:27 -0300

    Add gues the number

--- variables/main.go
+++ variables/main.go
@@ -1,14 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "pressione o enter"
 
 func main() {
-	var firstNumber int
-	firstNumber = 10
-	var secondNumber = 5
-	subtraction := 9
+	firstNumber := 2
+	secondNumber := 5
+	subtract := 7
+	//var answer int
 
-	fmt.Println(firstNumber)
-	fmt.Println(secondNumber)
-	fmt.Println(subtraction)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Guess the number")
+	fmt.Println("-----------------------")
+	fmt.Println(" ")
+	fmt.Println("Pense em um numero entre 1 e 10", prompt)
+	reader.ReadString('\n')
+	fmt.Println("Multiplique o seu numero por.", firstNumber, prompt)
+	reader.ReadString('\n')
+	fmt.Println("Agora faça pelo numero o seu numero.", secondNumber, prompt)
+	reader.ReadString('\n')
+	fmt.Println("Agora divida pelo resultado pelo numero que vc havia pensado enteriormente.")
+	reader.ReadString('\n')
+	fmt.Println("Now subtract", subtract, prompt)
+	reader.ReadString('\n')
 }
